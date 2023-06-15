6e93079d17692b4886b036243e961ece9d8c6004
Buldojik <Gaiwnik133777@gmail.com>
2023-06-15 14:50:08 +0300

    /

--- WebApplication3/Controllers/DivisionController.cs
+++ WebApplication3/Controllers/DivisionController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using WebApplication3.Connection;
 using WebApplication3.Models;
@@ -12,24 +10,22 @@ namespace WebApplication3.Controllers
     [Route("/api/[controller]")]
     public class DivisionController : ControllerBase
     {
-        //private List<Division> divisions = new List<Division>(new[] {
-        //    new Division{ ID = 1, Director = "fdsfdfs", Name = "dasdas" },
-        //    new Division{ ID = 2, Director = "Pavc", Name = "kontr" },
-        //    new Division{ ID = 3, Director = "Pavc", Name = "kontr" }
-        //});
-
         private readonly ConnectionContext _dataBase;
 
         public DivisionController(ConnectionContext dataBase)
         {
             _dataBase = dataBase;
         }
-
+        /// <summary>
+        /// Получает данные сущности     
+        /// </summary>
         [HttpGet]
         public IEnumerable<Division> Get() => _dataBase.Set<Division>();
-
+        /// <summary>
+        /// Получает данные сущности по ID     
+        /// </summary>
         [HttpGet("{id}")]
-        public async Task <IActionResult> GetID(int id)
+        public async Task<IActionResult> GetID(int id)
         {
             var division = await _dataBase.Set<Division>().FirstOrDefaultAsync(p => p.ID == id);
             if (division == null)
@@ -43,34 +39,20 @@ namespace WebApplication3.Controllers
         /// Удаляет данные сущности по ID      
         /// </summary>
         [HttpDelete("{id}")]
-        public async Task <IActionResult> Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             _dataBase.Remove(await _dataBase.Set<Division>().FirstOrDefaultAsync(p => p.ID == id));
             return Ok(new { Message = "Deleted" });
         }
-
+        /// <summary>
+        /// Заполняет сущность
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> Create(Division divisions)
         {
             _dataBase.Add(divisions);
             await _dataBase.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetID), new {id = divisions.ID}, divisions);
+            return CreatedAtAction(nameof(GetID), new { id = divisions.ID }, divisions);
         }
-        
-        //private int NextId => divisions.Count() == 0 ? 1 : divisions.Max(x => x.ID) + 1;
-
-        //[HttpPost]
-        //public IActionResult Post(Division division)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-        //    division.ID = NextId;
-        //    divisions.Add(division);
-        //    return CreatedAtAction(nameof(Get), new { id = division.ID }, division);
-        //}
-        //[HttpPost("AddDivision")]
-        //public IActionResult PostBody([FromBody] Division division) => Post(division);
     }
 }
